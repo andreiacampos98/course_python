8344532c325096a554b14ac31bf5db9e437d9eeb
andreiacampos98 <andreiacampos98.15@gmail.com>
2024-12-27 08:46:21 +0000

    evrything done

--- Day4/project.py
+++ Day4/project.py
@@ -1,11 +1,10 @@
 print("Welcome to Treasure island.\nYour mission is to find the treasure.")
-direction=input("Do you want to go to Left or Right?")
+direction=input('Do you want to go to "Left" or "Right"?')
 if(direction == 'left'):
-    way=input("Do you want to swim or wait?").lower()
+    way=input('Do you want to "swim" or "wait"?').lower()
     if (way == 'wait'):
-        door=input("Which door do you prefer: red, yellow or blue").lower()
+        door=input('Which door do you prefer: "red", "yellow" or "blue"').lower()
         if(door=='yellow'):
             print("You win!")
-        print("Game Over")
-    print("Game Over")
+            exit()
 print("Game Over")
